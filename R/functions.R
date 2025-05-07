836535273f2fbc88eb66647cabdbda03d3ec330b
Mette Kathrine Nygaard <mknygaard@gmail.com>
2025-05-07 13:23:23 +0200

    Import data and using regex extration participant ID

--- R/functions.R
+++ R/functions.R
@@ -1,4 +1,4 @@
-#' Import data from thr DIME study dataset
+#' Import data from the DIME study data set
 #'
 #' @param file_path Path to the CSV file
 #'
@@ -15,18 +15,42 @@ import_dime <- function(file_path) {
   return(data)
 }
 
-#' Import DIME csv files
+
+#' Import all DIME csv files in a folder into one data frame
 #'
-#' @param folder_path Path to DIME data
+#' @param folder_path The path to the folder that has the CSV files
 #'
-#' @returns Returns to all the data frame
+#' @returns A single data frame/tibble
 #'
 import_csv_files <- function(folder_path) {
   files <- folder_path %>%
     fs::dir_ls(glob = "*.csv")
+
   data <- files %>%
     purrr::map(import_dime) %>%
     purrr::list_rbind(names_to = "file_path_id")
-
   return(data)
+}
+
+
+#' A function with regex for ID extraction
+#'
+#' @param data Which data we want to look at
+#'
+#' @returns Data with regex
+#'
+get_participant_id <- function(data) {
+  data_with_id <- data %>%
+    dplyr::mutate(
+      id = stringr::str_extract(
+        file_path_id,
+        "[:digit:]+\\.csv$"
+      ) %>%
+        stringr::str_remove("\\.csv$") %>%
+        as.integer(),
+      .before = file_path_id
+    ) %>%
+    dplyr::select(-file_path_id)
+
+  return(data_with_id)
 }
