7ee5c91c29b6e7dcc5f318ff38df3c80e7178149
Mette Kathrine Nygaard <mknygaard@gmail.com>
2025-05-07 09:49:40 +0200

    Added a import csv file function

--- R/functions.R
+++ R/functions.R
@@ -14,3 +14,19 @@ import_dime <- function(file_path) {
 
   return(data)
 }
+
+#' Import DIME csv files
+#'
+#' @param folder_path Path to DIME data
+#'
+#' @returns Returns to all the data frame
+#'
+import_csv_files <- function(folder_path) {
+  files <- folder_path %>%
+    fs::dir_ls(glob = "*.csv")
+  data <- files %>%
+    purrr::map(import_dime) %>%
+    purrr::list_rbind(names_to = "file_path_id")
+
+  return(data)
+}
